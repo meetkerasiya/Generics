c2336453d0b56023ab2d9adf9192c80ad70e1cd6
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-17 15:57:44 +0530

    first commit

---
+++ Stack for doubles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Stack_for_doubles
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            StackDoubles();
+            StackStrings();
+            StackCollectionDouble();
+        }
+
+        private static void StackCollectionDouble()
+        {
+            var stack = new Stack<double>();
+            stack.Push(2.5);
+            stack.Push(3.8);
+            stack.Push(4.0);
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item = (double)stack.Pop();
+                Console.WriteLine("Item: " + item);
+                sum += item;
+            }
+            Console.WriteLine("Sum: " + sum);
+
+        }
+
+        private static void StackDoubles()
+        {
+            //var stack = new SimpleStackDouble();
+            var stack = new SimpleStack<double>();
+            stack.Push(2.5);
+            stack.Push(3.8);
+            stack.Push(4.0);
+            double sum = 0.0;
+            while (stack.Count > 0)
+            {
+                double item =(double) stack.Pop();
+                Console.WriteLine("Item: " + item);
+                sum += item;
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+        private static void StackStrings()
+        {
+           //var stack=new SimpleStackString();
+            var stack=new SimpleStack<string>();
+            stack.Push("Hello");
+            stack.Push("Hola"); 
+           
+            while (stack.Count > 0) 
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Stack for doubles/SimpleStack.cs
@@ -0,0 +1,59 @@
+﻿namespace Stack_for_doubles
+{
+    /*
+     //use of object instead of data type
+    public class SimpleStack
+    {
+        private readonly object[] _items;
+        private int _currentIndex = -1;
+        public SimpleStack() => _items = new object[10];
+
+        public int Count => _currentIndex + 1;
+        public void Push(object item) => _items[++_currentIndex] = item;
+
+        public object Pop() => _items[_currentIndex--];
+    }
+    */
+    /*
+    //duplication to create type safe
+    public class SimpleStackDouble
+
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+        public SimpleStackDouble() => _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+        public void Push(double item) => _items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+    }
+
+    public class SimpleStackString
+    {
+        private readonly string[] _items;
+        private int _currentIndex = -1;
+        public SimpleStackString() => _items = new string[10];
+
+        public int Count => _currentIndex + 1;
+        public void Push(string item) => _items[++_currentIndex] = item;
+
+        public string Pop() => _items[_currentIndex--];
+    }
+    */
+
+    //use Generics
+    //T is TypePlaceHolder
+    public class SimpleStack<T>
+    {
+        private readonly T[] _items;
+        private int _currentIndex = -1;
+        public SimpleStack() => _items = new T[10];
+
+        public int Count => _currentIndex + 1;
+        public void Push(T item) => _items[++_currentIndex] = item;
+
+        public T Pop() => _items[_currentIndex--];
+    }
+
+}
